4cb2429f22ea904c8d91b4e90209d43ac342b2a6
Michael Ruoss <michael@michaelruoss.ch>
2024-01-12 21:41:41 +0100

    use "normal" git_src

--- fava/entrypoint.sh
+++ fava/entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-if [  -n "${GIT_SRC}" ] && [ -d "${GIT_SRC}" ]; then
-    git clone "${GIT_SRC}" "${GIT_DST}" || true
-    cd "${GIT_DST}"
-    git pull
-fi
+echo yes | git clone "${GIT_SRC}" "${GIT_DST}" || true
+cd "${GIT_DST}"
+git pull
 
 exec $@
